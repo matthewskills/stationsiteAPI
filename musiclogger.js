c420d3905ea4c47e92fbfc317af1ebc385447cae
matthewskills <matt.troughear@gmail.com>
2020-11-12 15:35:10 +0000

    created music logger

--- musiclogger.js
+++ musiclogger.js
@@ -4,37 +4,44 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./database.sqlite');
 
 const musicLogger = () => {
-request({uri: "http://64.237.40.90:8232/7?sid=2"}, 
-    function(error, response, body) {
-            let remFrontTags = body.replace('<html><body>', '');
-            let remBackTags = remFrontTags.replace('</body></html>', '');
-            const data = remBackTags.split(',')
-            const listenerCount = data[0];
-            const song = data[6].split('-');
-            const artist = song[0].trim();
-            const title = song[1].trim();
-   
 
-    db.run(`INSERT INTO musiclogs (station_id,artist,title,listener_count,timestamp,as_album_art) VALUES ($stationId,$artist,$title,$listenerCount,$timestamp,$asAlbumArt)`, 
-    {
-        $stationId: 1,
-        $artist: artist,
-        $title: title,
-        $listenerCount: listenerCount,
-        $timestamp: 0,
-        $asAlbumArt: 0
-    },
-    function(err){
-        if(err) { next(err); } else {
-            console.log(listenerCount);
-    console.log(artist);
-    console.log(title);
-        }
-    }
-    
-    ) 
-  }
-);
-}
+db.all(`SELECT id,stream_url FROM stations`, (err,stations) => {
+
+    stations.forEach(station => {
+        
+        setTimeout(function() {
+            request({uri: station.stream_url+"/7?sid=2"}, 
+            function(error, response, body) {
+                let remFrontTags = body.replace('<html><body>', '');
+                let remBackTags = remFrontTags.replace('</body></html>', '');
+                const data = remBackTags.split(',')
+                const listenerCount = data[0];
+                const song = data[6].split('-');
+                const artist = song[0].trim();
+                const title = song[1].trim();
+
+                db.run(`INSERT INTO musiclogs (station_id,artist,title,listener_count,timestamp,as_album_art) VALUES ($stationId,$artist,$title,$listenerCount,$timestamp,$asAlbumArt)`, 
+                {
+                    $stationId: station.id,
+                    $artist: artist,
+                    $title: title,
+                    $listenerCount: listenerCount,
+                    $timestamp: 0,
+                    $asAlbumArt: 0
+                },
+                function(err){
+                    if(err) { next(err); } else {
+                        console.log('\x1b[33m%s\x1b[0m',`[MUSICLOGGER] `,'\x1b[0m',`station with id: ${station.id} is currently playing ${artist} - ${title} with ${listenerCount} listeners`)
+                    }
+                });
+        });
+        }, 1000)
+       
+
+    });
+
+})
+
+};
 
 module.exports = musicLogger;
